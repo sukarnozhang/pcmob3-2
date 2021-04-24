9fcbf79d981ddf90289c356df6fb7b9ad9686d09
Sukarno Sukarno <sukarno.zhang@gmail.com>
2021-04-24 11:39:39 +0800

    Task 4 completed

--- App.js
+++ App.js
@@ -1,54 +1,108 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import React, {useEffect} from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
+import { AntDesign } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("notes.db");
 
 function NotesScreen({ navigation }) {
+  const [notes, setNotes] = useState([
+    { title: "Walk the cat", done: false, id: "0" },
+    { title: "Feed the elephant", done: false, id: "1" },
+  ]);
 
   useEffect(() => {
     navigation.setOptions({
-      headerRight: () => <Button onPress={addColor} title="Add Color" />,
+      headerRight: () => (
+        // <AntDesign name="stepforward" size={24} color="black" onPress={console.log('a')} />,
+        // <Button onPress={console.log('a')} title="Add Color" />,
+
+        <TouchableOpacity onPress={addNote}>
+          <AntDesign name="stepforward" size={24} color="black" />
+        </TouchableOpacity>
+      ),
     });
   });
- return <View style={styles.container}></View>;
+
+  function addNote() {
+    let newNote = {
+      title: "Sample new note",
+      done: false,
+      id: notes.length.toString(),
+    };
+    setNotes([...notes, newNote]);
+  }
+
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={{ width: "100%" }}
+        data={notes}
+        renderItem={renderItem}
+      />
+    </View>
+  );
 }
 
 const Stack = createStackNavigator();
 
 export default function App() {
- return (
-   <NavigationContainer>
-     <Stack.Navigator>
-       <Stack.Screen
-         name="Notes"
-         component={NotesScreen}
-         options={{
-           headerTitle: "Notes App",
-           headerTitleStyle: {
-             fontWeight: "bold",
-             fontSize: 60,
-           },
-           headerStyle: {
-             height: 120,
-             backgroundColor: "yellow",
-             borderBottomColor: "#ccc",
-             borderBottomWidth: 1,
-           },
-         }}
-       />
-     </Stack.Navigator>
-   </NavigationContainer>
- );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Notes"
+          component={NotesScreen}
+          options={{
+            headerTitle: "Notes App",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              fontSize: 0,
+            },
+            headerStyle: {
+              height: 120,
+              backgroundColor: "yellow",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: "#ffc",
-   alignItems: "center",
-   justifyContent: "center",
- },
+  container: {
+    flex: 1,
+    backgroundColor: "#ffc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
-
-
