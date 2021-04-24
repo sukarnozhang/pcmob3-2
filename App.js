1e36fd4353025e1618853ad822da8c8ad57587cd
Sukarno Sukarno <sukarno.zhang@gmail.com>
2021-04-24 10:33:03 +0800

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import React, {useEffect} from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
+
+function NotesScreen({ navigation }) {
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Button onPress={addColor} title="Add Color" />,
+    });
+  });
+ return <View style={styles.container}></View>;
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen
+         name="Notes"
+         component={NotesScreen}
+         options={{
+           headerTitle: "Notes App",
+           headerTitleStyle: {
+             fontWeight: "bold",
+             fontSize: 60,
+           },
+           headerStyle: {
+             height: 120,
+             backgroundColor: "yellow",
+             borderBottomColor: "#ccc",
+             borderBottomWidth: 1,
+           },
+         }}
+       />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: "#ffc",
+   alignItems: "center",
+   justifyContent: "center",
+ },
 });
+
+
